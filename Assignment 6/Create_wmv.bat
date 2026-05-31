0488fe9e56490b39b05f7cc0b74a59f439ddbea7
JordanIIII <129195322+JordanIIII@users.noreply.github.com>
2026-05-31 13:33:38 +0200

    videos/gifs

---
+++ Assignment 6/Create_wmv.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\20-sim 5.1\video\mplayer\mencoder" mf://@0_list.txt -ovc lavc -lavcopts vcodec=wmv2 -mf fps=25:type=png -of lavf -lavfopts format=wmv -nosound -o "0.wmv"
+explorer "C:\Users\Jordan\OneDrive - University of Twente\Documents\Robotics Master\Modeling-and-Simulation\Assignment 6"
+"C:\Program Files (x86)\20-sim 5.1\video\mplayer\mplayer" "C:\Users\Jordan\OneDrive - University of Twente\Documents\Robotics Master\Modeling-and-Simulation\Assignment 6\0.wmv"
